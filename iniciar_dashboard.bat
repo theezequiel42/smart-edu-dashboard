f7c3de947cc889395afdef708539ef7fbd222ee0
EZEQUIEL ANTUNES DE LIMA <94005353+theezequiel42@users.noreply.github.com>
2025-03-10 20:16:57 -0300

    Atualiza script de inicialização do dashboard para usar o diretório atual e adiciona pausa

--- iniciar_dashboard.bat
+++ iniciar_dashboard.bat
@@ -1,3 +1,4 @@
 @echo off
-cd /d \\servidor\aplicacoes\smart-edu-dashboard
-python -m streamlit run main.py --server.address=0.0.0.0 --server.port=8501
+cd /d %~dp0
+python -m streamlit run main.py
+pause
